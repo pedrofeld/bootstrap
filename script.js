63de5309fa584375c5991c3833cd0c6858ec8128
pedrofeld <pedro.gael.feld@gmail.com>
2025-04-03 22:28:31 -0300

    adição de botão para alterar entre temas dark e light

--- script.js
+++ script.js
@@ -17,3 +17,27 @@ form.addEventListener('submit', event => {
         console.log(usuario)
     }
 });
+
+const btnTheme = document.getElementById("btn-theme");
+const icone = document.getElementById("icone-tema");
+let temaAtual = "light";
+
+btnTheme.addEventListener("click", () => {
+    switch(temaAtual){
+        case "light":
+            temaAtual = "dark";
+            document.body.setAttribute("data-bs-theme", "dark");
+            icone.setAttribute("class", "bi bi-sun-fill");
+            btnTheme.classList.remove("btn-dark");
+            btnTheme.classList.add("btn-light");
+            break;
+
+        case "dark":
+            temaAtual = "light";
+            document.body.setAttribute("data-bs-theme", "light");
+            icone.setAttribute("class", "bi bi-moon-stars-fill");
+            btnTheme.classList.remove("btn-light");
+            btnTheme.classList.add("btn-dark");
+            break;
+    }
+});
